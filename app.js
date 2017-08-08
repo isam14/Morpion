2ecf7b3df5891d661fc81d5c9e7b7a5e187cfdaf
isam14 <imuck14@gmail.com>
2017-08-08 10:31:37 +0200

    morpion non termine

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // function rond(a) {
     //      return a.html('<img src="circle.jpg" alt="rond">')
     // };
+
+$("#myModal").modal("show");
+
+    $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+
     var star = ($(this).html('<img src="star.png" alt="star">'));
     // var caseVide = 0;
     var count = 0;
@@ -24,10 +32,59 @@
         }
     });
     
+
+
+
+var combiWin = {
+    11 : [[11, 12, 13], [11, 21, 31], [11, 22, 33]],
+    12 : [[11, 12, 13], [12, 22, 32]],
+    13 : [[13, 22, 31], [13, 23, 33], [11, 12, 13]],
+    21 : [[11, 21, 31], [21, 22, 23]],
+    22 : [[11, 22, 33], [12, 22, 32], [13, 22, 31], [21, 22, 23]],
+    23 : [[21, 22, 23], [13, 23, 33]],
+    31 : [[13, 22, 31], [31, 32, 33], [11, 21, 31]],
+    32 : [[31, 32, 33], [12, 22, 32]],
+    33 : [[11,22,33], [13, 23, 33], [31, 32, 33]]
+}
+
+var joueur1 = ($("#Username").add("#joueur1").val());
+var joueur2 = 2;
+var currentId = this.id; //<-----pareil que var currentId = ($(this).attr("id"));
+var partieGagnante = combiWin = [currentId];
+
+
+function addPlayer()
+{
+    $("#Username1").text("#joueur1"); 
+}
+
+function testGagnant() {
+if (condition) 
+    {
+   console.log("hi");
     
+}
+
+
+//dès que je trouve une combiWin :
+return true;
+
+}
+
+
+
+
     $("#vider").click(function() {
         $(".case").empty();
+        count = 0;
     });
+
+
+
+
+
+
+
     
     
     // $(".case").click(function(){
